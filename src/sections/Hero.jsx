358b425db21107b21c70a540225f242b08179e1b
Tse-Heng-Hsueh <tylerxueofficial@gmail.com>
2025-08-03 15:18:41 -0500

    Initial commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { words } from "../constants";
+import Button from "../components/Button";
+import HeroExperience from "../components/models/hero_models/HeroExperience";
+
+const Hero = () => {
+  return (
+    <section id="hero" className="relative overflow-hidden">
+      <div className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt="background" />
+      </div>
+      <div className="hero-layout">
+        {/* Left: Hero Content */}
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                Turning
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+              <h1>into Scalable Solutions</h1>
+              <h1>that Deliver Real Impact</h1>
+            </div>
+            <p className="ttext-white-50 md:text-xl relative z-10">
+              Hi, I'm Tyler, A data enthusiast crafting GenAI tools and scalable
+              data systems.
+            </p>
+            <Button
+              text="See My Work"
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="counter"
+            />
+          </div>
+        </header>
+        {/* RIGHT: 3D Model or Visual */}
+        <figure>
+          <div className="hero-3d-layout">
+            <HeroExperience />
+          </div>
+        </figure>
+      </div>
+      <div className="hero-counter">{/*  */}</div>
+    </section>
+  );
+};
+
+export default Hero;
